9fc640a41ed614895e98226c1a15eed8e5fab197
Zach Price <zach.price105@gmail.com>
2025-10-21 12:18:00 -0700

    Add metadata for SEO and social sharing in layout.tsx
    - Updated metadata object to include openGraph and Twitter card details for better visibility on social platforms.
    - Added favicon and apple touch icon for improved branding.
    - Introduced new images for open graph sharing.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,8 +14,34 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://marketlens.zachprice.info'),
   title: "MarketLens | Stock Insights Dashboard",
   description: "See the markets clearly with stock data insights.",
+  openGraph: {
+    title: "MarketLens",
+    description: "See the markets clearly with stock data insights.",
+    url: "https://marketlens.zachprice.info",
+    siteName: "MarketLens",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "MarketLens",
+    description: "See the markets clearly with stock data insights.",
+    images: ["/opengraph-image.png"],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
